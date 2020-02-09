d7b486e5724c5c75061a39a26133e1e2841c7c28
Ayush23Dash <ayushshanker23@gmail.com>
2020-02-09 20:55:44 +0530

    Added about me section

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+
   var toggle = document.getElementById("toggle")
   toggle.innerHTML="<img src=\"https://www.solodev.com/assets/anchor/arrow-down.png\" width=\"20px\" height=\"25px\">";
 
@@ -16,3 +18,19 @@ function frameLooper()
   loopTimer = setTimeout('frameLooper()',130);
 }
 frameLooper();
+
+
+jQuery(document).ready(function($) {
+  // Scroll to the desired section on click
+  // Make sure to add the `data-scroll` attribute to your `<a>` tag.
+  // Example:
+  // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
+  function scrollToSection(event) {
+    event.preventDefault();
+    var $section = $($(this).attr('href'));
+    $('html, body').animate({
+      scrollTop: $section.offset().top
+    }, 500);
+  }
+  $('[data-scroll]').on('click', scrollToSection);
+}(jQuery));
